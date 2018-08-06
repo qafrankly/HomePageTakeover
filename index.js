71f94a487f4b6de27eddf67e0bae04ba2037ea6f
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-08-06 15:47:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-{
-"type": "row",
-"gutterSpacing": "",
-"cols": [{
-"type": "col",
-"spans":
+import React, { Component, PropTypes } from 'react';
 
-{ "xl": 12, "lg": 12, "md": 12, "sm": 12, "xs": 12 }
-,
-"gutterSpacing": "xs",
-"components": [{
-"id": "components/custom/ssr/HomePageTakeover",
-"affiliates": [
-"kotv",
-"kwtv"
-],
-"FRN_resourceEndpoints": [
+class RandomTest extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      h2 = 'sup this a github edit',
+    } = this.props;
 
-{ "url": "http://www.newson6.com/category/331247/school-shutdown-feed?clienttype=container.json", "type": "json" }
-],
-"FRN_style": {},
-"visualInfo":
+    let {
+      flux, config
+    } = this.context;
 
-{ "affiliate": "kotv" }
-,
-"props":
-
-{ "affiliate": "kotv", "origin": "http://www.newson6.com/category/331247/school-shutdown-feed", "title": "", "photoUrl": "http://KOTV.images.worldnow.com/images/16432230_G.jpg" }
-}]
-}]
+    return (
+      <div>
+        <h2>{h2}</h2>
+      </div>
+    );
+  }
 }
+
+export default RandomTest;
